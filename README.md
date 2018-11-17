327a8f2d5c57aaa640c03b2cdb6bfe7ad5caba9c
Kevin Hubbard <dgkallday98@gmail.com>
2018-11-16 19:55:03 -0500

    updated readme and commented js file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# tempConverter
-Fahrenheit to Celsius
+# Temperature Converter
+### Web app for converting between fahrenheit and celsius.
+
+##### Things i'd like to add.
+* Kelvin
+* other units of conversion
+..* inches/feet to meeters
+
+
--- scripts/converter.js
+++ scripts/converter.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-
+	//adds label for current unit
 	$('#fahrenheit').change(function(e) {
 		e.preventDefault();
 		$('#test').empty();
 		$('#test').append('&deg; Fahrenheit');
 	});
-
+	//adds label for current unit
 	$('#celsius').change(function(e) {
 		e.preventDefault();
 		$('#test').empty();
@@ -14,13 +14,13 @@ $(document).ready(function() {
 
 
 
-
-	$('form').on('submit', function(e){
+	//form checks what unit to convert
+	$('form').on('submit', function(e) {
 		e.preventDefault();
 		//condition to check whether celsius or fahrenheit has been selected
-		if($('#fahrenheit').prop('checked')){
+		if ($('#fahrenheit').prop('checked')) {
 			calculateCelsius();
-		} else if ($('#celsius').prop('checked')){
+		} else if ($('#celsius').prop('checked')) {
 			calculateFahrenheit();
 		} else {
 			console.log('something went wrong');
@@ -29,25 +29,31 @@ $(document).ready(function() {
 	});
 
 
-function calculateCelsius(){
+	function calculateCelsius() {
+		//clears div
 		$('#output').empty();
 		$('#unit').empty();
+		//gets user input
 		var input = $('#degree').val();
+		//parses user input and does celsius conversion
+		var celsius = (parseInt(input) - 32) * (5 / 9);
+		//append output to div
+		$('#unit').append('&deg;' + 'C');
+		$('#output').append(celsius.toFixed(1));
+	}
 
-		var celsius = (parseInt(input) - 32) * (5/9);
-		$('#unit').append('&deg;'+'C');
-		$('#output').append(celsius.toFixed(1));		
-}
-
-function calculateFahrenheit(){
-	$('#output').empty();
-	$('#unit').empty();
-	var input = $('#degree').val();
-
-	var fahrenheit = (parseInt(input) * (9/5)) + 32;
-	$('#unit').append('&deg;'+'F');
-	$('#output').append(fahrenheit);
-}
+	function calculateFahrenheit() {
+		//clears div
+		$('#output').empty();
+		$('#unit').empty();
+		//gets user input
+		var input = $('#degree').val();
+		//parses user input and does fahrenheit conversion
+		var fahrenheit = (parseInt(input) * (9 / 5)) + 32;
+		//append output to div
+		$('#unit').append('&deg;' + 'F');
+		$('#output').append(fahrenheit);
+	}
 
 
 });
